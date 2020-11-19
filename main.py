383628a763b86f211a3fef9338c0183e75982caa
Greg Hawthorne <ghawthorne@me.com>
2020-11-19 07:01:48 +0100

    square

---
+++ main.py
@@ -0,0 +1,35 @@
+
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+# timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("blue")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+
+# timmy_the_turtle.backward(200)
+# timmy_the_turtle.left(180)
+# timmy_the_turtle.setheading(0)
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
